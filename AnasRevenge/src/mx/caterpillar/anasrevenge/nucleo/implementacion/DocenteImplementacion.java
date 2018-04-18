0b49f81152558a281f8ea2ea35ea413ed4c33c95
OsmarElProgramador <barrazita11@gmail.com>
2018-04-18 16:20:19 -0700

    Inicio

---
+++ AnasRevenge/src/mx/caterpillar/anasrevenge/nucleo/implementacion/DocenteImplementacion.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.caterpillar.anasrevenge.nucleo.implementacion;
+
+import mx.caterpillar.anasrevenge.nucleo.interfaces.IDocente;
+
+/**
+ *
+ * @author osmar
+ */
+public class DocenteImplementacion implements IDocente{
+    
+}
