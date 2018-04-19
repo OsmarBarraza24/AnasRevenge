f2d9917e080e20f21fd266f6c6787d149e8041fe
OsmarElProgramador <barrazita11@gmail.com>
2018-04-18 19:47:11 -0700

    DocenteImplementacion

--- AnasRevenge.Nucleo/src/mx/caterpillar/anasrevenge/nucleo/implementacion/DocenteImplementacion.java
+++ AnasRevenge.Nucleo/src/mx/caterpillar/anasrevenge/nucleo/implementacion/DocenteImplementacion.java
@@ -5,12 +5,92 @@
  */
 package mx.caterpillar.anasrevenge.nucleo.implementacion;
 
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import mx.caterpillar.anasrevenge.nucleo.entidades.Docente;
+import mx.caterpillar.anasrevenge.nucleo.entidades.HibernateUtil;
 import mx.caterpillar.anasrevenge.nucleo.interfaces.IDocente;
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
 
 /**
  *
  * @author osmar
  */
 public class DocenteImplementacion implements IDocente{
+
+    @Override
+    public List<Docente> getAll() {
+        List<Docente> docentes = new ArrayList<Docente>();
+        try {
+            Session session = HibernateUtil.getSessionFactory().openSession();
+            CriteriaBuilder builder = session.getCriteriaBuilder();
+            CriteriaQuery<Docente> criteria = builder.createQuery(Docente.class);
+            criteria.from(Docente.class);
+            docentes = session.createQuery(criteria).getResultList();
+            session.close();
+        } catch (Exception ex) {
+            System.out.println("Ocurrió un error al intentar obtener registros de Compañía. Descripción: " + ex.getMessage());
+        }
+        return docentes;
+    }
+
+    @Override
+    public Docente getByUID(String uid) {
+        Docente docente = new Docente();
+        try {
+            Session session = HibernateUtil.getSessionFactory().openSession();
+            CriteriaBuilder builder = session.getCriteriaBuilder();
+            CriteriaQuery<Docente> criteria = builder.createQuery(Docente.class);
+            criteria.from(Docente.class);
+            docente = (Docente)session.createCriteria(Docente.class).add(Restrictions.eq("uid", uid)).list().get(0);
+            session.close();
+        } catch (Exception e) {
+        }
+        return docente;
+    }
+
+    @Override
+    public void save(Docente docente) {
+        try {
+            Session session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            session.save(docente);
+            session.getTransaction().commit();
+            session.close();
+        } catch (Exception e) {
+            System.out.println("Ocurrió un error al intentar guardad el objeto de tipo usuario");
+        }
+    }
+
+    @Override
+    public void update(Docente docente) {
+        try {
+            Session session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            session.update(docente);
+            session.getTransaction().commit();
+            session.close();
+        } catch (Exception e) {
+            System.out.println("Ocurrió un error al intentar guardad el objeto de tipo usuario");
+        }
+    }
+
+    @Override
+    public void delete(int id) {
+        Docente docente = new Docente();
+        docente.setId(id);
+        try {
+            Session session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            session.delete(docente);
+            session.getTransaction().commit();
+            session.close();
+        } catch (Exception e) {
+            System.out.println("Ocurrió un error al intentar guardad el objeto de tipo usuario");
+        }
+    }
     
 }
--- AnasRevenge.Nucleo/src/mx/caterpillar/anasrevenge/nucleo/interfaces/IDocente.java
+++ AnasRevenge.Nucleo/src/mx/caterpillar/anasrevenge/nucleo/interfaces/IDocente.java
@@ -5,10 +5,17 @@
  */
 package mx.caterpillar.anasrevenge.nucleo.interfaces;
 
+import java.util.List;
+import mx.caterpillar.anasrevenge.nucleo.entidades.Docente;
+
 /**
  *
  * @author osmar
  */
 public interface IDocente {
-    
+    public List<Docente> getAll();
+    public Docente getByUID(String uid);
+    public void save(Docente docente);
+    public void update(Docente docente);
+    public void delete(int id);
 }
